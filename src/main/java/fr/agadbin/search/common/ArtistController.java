048d22b118ee3d480f6897ad1a0c2386a6b97368
Alexis GADBIN <agadbin@wideagency.com>
2024-06-05 14:03:12 +0200

    fix(Artist Controller): remove cross origin protection

--- src/main/java/fr/agadbin/search/common/ArtistController.java
+++ src/main/java/fr/agadbin/search/common/ArtistController.java
@@ -2,6 +2,7 @@ package fr.agadbin.search.common;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin
 @RequestMapping("/artists")
 public class ArtistController {
 
