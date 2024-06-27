fb315589271744e580af736609c274cc16a6c57c
Alexis GADBIN <agadbin@wideagency.com>
2024-06-27 16:13:07 +0200

    feat(home page): client quick update

--- client/src/app/[locale]/page.tsx
+++ client/src/app/[locale]/page.tsx
@@ -1,29 +1,42 @@
 import SwitchLanguage from '@components/switch-language.component'
-import { Button } from '@components/ui/button'
 import ThemeToggle from '@src/components/theme-toggle.component'
 import { locales } from '@src/middleware'
 import { themes } from '@src/utils/constants'
 import { useTranslations } from 'next-intl'
+import Link from 'next/link'
 
 export default function Home() {
   const t = useTranslations()
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center gap-16 p-24">
       <h1>{t('site.title')}</h1>
-      <SwitchLanguage
-        options={locales.map((locale) => ({
-          value: locale,
-          label: t(`language.${locale}`),
-        }))}
-      />
-      <ThemeToggle
-        options={themes.map((theme) => ({
-          value: theme,
-          label: t(`theme.${theme}`),
-        }))}
-      />
-      <Button>Click me</Button>
+      <div>
+        <p>{t('site.description')}</p>
+        <Link
+          href="https://www.kaggle.com/datasets/momanyc/museum-collection/data"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="text-blue-500 underline"
+        >
+          {t('site.source')}
+        </Link>
+        <p>{t('site.explanation')}</p>
+      </div>
+      <div className="flex gap-4">
+        <SwitchLanguage
+          options={locales.map((locale) => ({
+            value: locale,
+            label: t(`language.${locale}`),
+          }))}
+        />
+        <ThemeToggle
+          options={themes.map((theme) => ({
+            value: theme,
+            label: t(`theme.${theme}`),
+          }))}
+        />
+      </div>
     </main>
   )
 }
